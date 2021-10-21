893f98cefade4345472af8bb84bf7f6495085471
Tom Emv <mobineko@inbox.ru>
2021-10-21 11:15:06 +0300

    version for multiple

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Choco Package Updater
+
+
 import re
 import subprocess
 
@@ -8,18 +14,18 @@ from jinja2 import Template
 from loguru import logger
 
 
-with open("settings/config.yaml", "r") as ymlfile:
+with open("settings/config2.yaml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
 
-logger.add("logs/debug.log", level="DEBUG", format="{time} {level} {message}", rotation="1 MB", compression="zip")
+if cfg['config']['logging']['file_log_enabled']:
+    logger.add(cfg['config']['logging']['file_log_path'], level="DEBUG", format="{time} {level} {message}", rotation="16 KB", compression="zip")
 
 
 class Program:
-    def __init__(self, owner, repo):
-        self.owner = owner
-        self.repo = repo
-        self.response_git = requests.get(f"https://api.github.com/repos/{owner}/{repo}/releases/latest")
-        self.response_choco = requests.get(f"https://community.chocolatey.org/packages/{repo}")
+    def __init__(self, app):
+        self.app = app
+        self.response_git = requests.get(f"https://api.github.com/repos/{self.app['owner']}/{self.app['repo']}/releases/latest")
+        self.response_choco = requests.get(f"https://community.chocolatey.org/packages/{self.app['repo']}")
 
     def get_git_ver(self):
         git_ver = self.response_git.json()["tag_name"]
@@ -28,10 +34,10 @@ class Program:
     def get_choco_ver(self):
         soup = BeautifulSoup(self.response_choco.text, "lxml")
         page_items = soup.find_all("span", class_="ms-2")
-        choco_ver = (re.search(cfg["regexp_mask"]["choco_ver"], str(page_items))).group(0)
+        choco_ver = (re.search(self.app["regexp_mask"]["choco_ver"], str(page_items))).group(0)
         return choco_ver
 
-    def form_templatevars(self):
+    def form_template_vars(self):
         windows = list(
             filter(
                 lambda platform: platform["content_type"] == "application/x-ms-dos-executable",
@@ -41,20 +47,31 @@ class Program:
         url_32 = ((windows)[0])["browser_download_url"]
         url_64 = ((windows)[1])["browser_download_url"]
 
-        checksum_32 = (re.search(cfg["regexp_mask"]["checksum_32"], str(self.response_git.json()["body"]))).group(1)
-        checksum_64 = (re.search(cfg["regexp_mask"]["checksum_64"], str(self.response_git.json()["body"]))).group(1)
+        checksum_32 = (re.search(self.app["regexp_mask"]["checksum_32"], str(self.response_git.json()["body"]))).group(1)
+        checksum_64 = (re.search(self.app["regexp_mask"]["checksum_64"], str(self.response_git.json()["body"]))).group(1)
 
-        changelog = (re.search(cfg["regexp_mask"]["changelog_prepare"], str(self.response_git.json()["body"]))).group(0)
-        changelog = re.findall(cfg["regexp_mask"]["changelog_final"], changelog, re.MULTILINE)
+        changelog = (re.search(self.app["regexp_mask"]["changelog_prepare"], str(self.response_git.json()["body"]))).group(0)
+        changelog = re.findall(self.app["regexp_mask"]["changelog_final"], changelog, re.MULTILINE)
         changelog = str("\n".join(changelog))
 
         dict_provision = {
+            "id": self.app["id"],
+            "title": self.app["title"],
+            "authors": self.app["authors"],
+            "projectUrl": self.app["projectUrl"],
+            "iconUrl": self.app["iconUrl"],
+            "licenseUrl": self.app["licenseUrl"],
+            "tags": self.app["tags"],
+            "summary": self.app["summary"],
+            "description": self.app["description"],
             "url_32": url_32,
             "url_64": url_64,
+            "checksumType": self.app["checksumType"],
             "checksum_32": checksum_32,
             "checksum_64": checksum_64,
+            "silentArgs": self.app["silentArgs"],
             "version": self.get_git_ver(),
-            "changelog": changelog,
+            "changelog": changelog
         }
 
         return dict_provision
@@ -75,16 +92,18 @@ def run_command(command):
 
 
 if __name__ == "__main__":
-    target_program = Program(cfg["program"]["owner"], cfg["program"]["repo"])
+    for app_name, app in cfg['config']['programs'].items():
+        target_program = Program(app)
+        # print(target_program.app)
 
-    if target_program.get_git_ver() == target_program.get_choco_ver():
-        print("No updates. Time to drunch!")
-    else:
-        print("Oh no! Time to new build! Go ahead!")
+        if target_program.get_git_ver() == target_program.get_choco_ver():
+            print("No updates. Time to drunch!")
+        else:
+            print("Oh no! Time to new build! Go ahead!")
+            
+            dict_provision = target_program.form_template_vars()
+            write_manifests("chocolateyinstall.template", "fitter/tools/chocolateyinstall.ps1")
+            write_manifests("nuspec.template", f"fitter/{target_program.app['repo'].lower()}.nuspec")
 
-        dict_provision = target_program.form_templatevars()
-        write_manifests("chocolateyinstall.template", "fitter/tools/chocolateyinstall.ps1")
-        write_manifests("nuspec.template", f"fitter/{target_program.repo.lower()}.nuspec")
-
-        logger.debug(run_command("cd fitter/ && choco pack"))
-        # run_command(f'cd fitter/ && choco push {target_program.repo.lower()}.{git_ver}.nupkg --source https://push.chocolatey.org/') # uncomment for prod
+            logger.debug(run_command("cd fitter/ && choco pack"))
+            # run_command(f'cd fitter/ && choco push {target_program.repo.lower()}.{git_ver}.nupkg --source https://push.chocolatey.org/') # uncomment for prod
